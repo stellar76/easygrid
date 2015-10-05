b81b8ffb47d07439fbd2f55226874661def70d05
Jody Christain <jodychristian@gmail.com>
2015-10-05 13:01:20 -0500

    update.
    added mobile menu and search functions

--- js/easygrid.js
+++ js/easygrid.js
@@ -1,1 +1,19 @@
-//jodework js
+//easygrid js
+var navtoggled = 'off';
+$(function() {
+    $('#navbutton').click(function() {
+        if (navtoggled == 'off') {
+            $(this).empty();
+            $(this).html('<img src="img/menu_x.png"/>');
+            $(this).addClass('open');
+            $('topnav ul').addClass('open');
+            navtoggled = 'on';
+        } else if (navtoggled == 'on') {
+            $(this).empty();
+            $(this).html('<img src="img/menu.png"/>');
+            $(this).removeClass('open');
+            $('topnav ul').removeClass('open');
+            navtoggled = 'off';
+        }
+    });
+});
